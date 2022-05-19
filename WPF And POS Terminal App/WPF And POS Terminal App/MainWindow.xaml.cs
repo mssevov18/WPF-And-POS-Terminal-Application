35d5a619c91cfb57e432bfaa73a7610ff90f65e2
mssevov18 <mssevov18@codingburgas.bg>
2022-05-19 11:06:22 +0300

    Create App

---
+++ WPF And POS Terminal App/WPF And POS Terminal App/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Threading;
+
+namespace WPF_And_POS_Terminal_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SerialPort port;
+        private System.Windows.Threading.DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            port = new SerialPort();
+            port.BaudRate = 9600;
+            port.PortName = "COM5";
+            port.Open();
+
+            InitializeComponent();
+
+        }
+
+        private void click(object sender, EventArgs e)
+        {
+            string line = port.ReadLine();
+            COMTextBox.Text = line;
+            if (line == "True")
+                COMRectangle.Fill = Brushes.Green;
+            else if (line == "False")
+                COMRectangle.Fill = Brushes.Yellow;
+            else
+                COMRectangle.Fill = Brushes.Red;
+        }
+    }
+}
